8e84743b6df834e3cf99bc29c18f9d21b9d28e3a
Erik Anderson <eanderson@enviroincentives.com>
2021-05-21 11:15:50 -0600

    switch to deployment database

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import dash
 import dash_bootstrap_components as dbc
+import os
 
 
 external_stylesheets = [dbc.themes.YETI]
@@ -11,7 +12,7 @@ app = dash.Dash(__name__, server=server,
                 suppress_callback_exceptions=True,
                 external_stylesheets=external_stylesheets)
 
-ENV = 'dev'
+ENV = 'deploy'
 
 if ENV == 'dev':
     app.server.debug = True
@@ -19,7 +20,7 @@ if ENV == 'dev':
         'postgresql://postgres:incentives@localhost/test2'
 else:
     app.server.debug = False
-    app.server.config['SQLALCHEMY_DATBASE_URI'] = ''
+    app.server.config['SQLALCHEMY_DATBASE_URI'] = os.environ['DATABASE_URL']
 
 # avoids warning in console
 app.server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
